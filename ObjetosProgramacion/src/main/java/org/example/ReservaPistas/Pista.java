f9e5cb17ec119c6cea1c072b12993873827263ec
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2026-03-17 10:59:53 +0100

    Practica de Reserva de pistas

---
+++ ObjetosProgramacion/src/main/java/org/example/ReservaPistas/Pista.java
@@ -0,0 +1,16 @@
+package org.example.ReservaPistas;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+
+
+@Getter
+@ToString
+@AllArgsConstructor
+public abstract class Pista {
+    private int id;
+    private String techado;
+
+}
---
+++ ObjetosProgramacion/src/main/java/org/example/ReservaPistas/ReservaApp.java
@@ -0,0 +1,43 @@
+package org.example.ReservaPistas;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class ReservaApp {
+    static Scanner teclado = new Scanner(System.in);
+    static HashMap<Usuario, LinkedHashSet<Reserva>> mapa = new HashMap<>();
+
+    static void main() {
+
+    }
+    public static void eliminarReserva(LinkedHashSet<Reserva> listaReservas){
+        Iterator<Reserva> it = listaReservas.iterator();
+        System.out.println("Cual borramos");
+        String opcion = teclado.next().toUpperCase();
+
+        while (it.hasNext()){
+            Reserva reserva = it.next();
+            if (opcion.equals("PADEL") && reserva.getPista() instanceof Padel){
+                it.remove();
+            } else if (opcion.equals("TENIS") && reserva.getPista() instanceof Tenis) {
+                it.remove();
+            }
+        }
+    }
+    public static LinkedHashSet<Reserva> obtenerReservas (Usuario usuario){
+        return mapa.get(usuario);
+    }
+    public static void crearResrva(Usuario usuario, Pista pista) {
+        Reserva reserva = new Reserva(usuario, pista);
+
+        LinkedHashSet<Reserva> listaReservas = obtenerReservas(usuario);
+        if (listaReservas.contains(reserva)) {
+            throw new ExcepcionReservaDuplicada();
+        } else {
+            listaReservas.add(reserva);
+            mapa.put(usuario, listaReservas);
+        }
+    }
+}
