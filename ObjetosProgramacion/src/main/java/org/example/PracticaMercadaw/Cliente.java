2d41d1abdbb5ec16025319f814126d02a09502a4
Ivan <ramosgamarraivanjose@yahoo.es>
2026-03-20 19:03:13 +0100

    comienzo practic mercadona

---
+++ ObjetosProgramacion/src/main/java/org/example/PracticaMercadaw/Cliente.java
@@ -0,0 +1,67 @@
+package org.example.PracticaMercadaw;
+
+public class Cliente {
+    private String usuario;
+    private String contrasenya;
+    private String direccion;
+    private boolean promociones;
+    private Pedido pedido;
+
+
+    public Cliente(String usuario, String contrasenya) {
+        this.usuario = usuario;
+        this.contrasenya = contrasenya;
+        this.pedido=null;
+        this.direccion="Calle Falsa 123";
+        this.promociones=false;
+    }
+
+    public void crearPedido(){
+
+    }
+    public void insertarProducto(Producto producto){
+
+    }
+
+
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isPromociones() {
+        return promociones;
+    }
+
+    public void setPromociones(boolean promociones) {
+        this.promociones = promociones;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
---
+++ ObjetosProgramacion/src/main/java/org/example/PracticaMercadaw/Mercadaw.java
@@ -0,0 +1,19 @@
+package org.example.PracticaMercadaw;
+
+import java.util.LinkedHashSet;
+
+public class Mercadaw {
+    LinkedHashSet<Cliente>listaClientes = new LinkedHashSet<>();
+
+    public Mercadaw(){
+
+    }
+
+    public void generarClientes(){
+
+    }
+
+    public LinkedHashSet<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+}
