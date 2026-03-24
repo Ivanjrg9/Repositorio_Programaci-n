3223f5628f7c8039ee0b86818d5ba5ac19354ebb
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2026-03-24 11:42:44 +0100

    pistas de padel borradas

--- ObjetosProgramacion/src/main/java/org/example/ReservaPistas/ExcepcionReservaDuplicada.java
+++
@@ -1,7 +0,0 @@
-package org.example.ReservaPistas;
-
-public class ExcepcionReservaDuplicada extends RuntimeException {
-    public ExcepcionReservaDuplicada() {
-        super("Esta reserva ya esta cogida");
-    }
-}
