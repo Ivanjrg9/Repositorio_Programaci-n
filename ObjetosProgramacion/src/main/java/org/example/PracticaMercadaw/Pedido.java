dcc528196ce4a6d1064a6a70cc2eb01a5283a7ad
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2026-03-25 11:08:02 +0100

    pc

--- ObjetosProgramacion/src/main/java/org/example/PracticaMercadaw/Pedido.java
+++ ObjetosProgramacion/src/main/java/org/example/PracticaMercadaw/Pedido.java
@@ -1,39 +1,39 @@
-//package org.example.PracticaMercadaw;
-//
-//import java.util.HashMap;
-//
-//public class Pedido {
-//    HashMap<Producto,Integer>mapaPedido = new HashMap<>();
-//    private double precio;
-//
-//    public Pedido() {
-//        this.mapaPedido = new HashMap<>();
-//        this.precio = 0;
-//    }
-//
-//    public void actualizarImporteTotal(double importe){
-//
-//    }
-//    public void aplicarPromo3x2(){
-//
-//    }
-//    public void aplicarPromo10(){
-//
-//    }
-//
-//    public HashMap<Producto, Integer> getMapaPedido() {
-//        return mapaPedido;
-//    }
-//
-//    public void setMapaPedido(HashMap<Producto, Integer> mapaPedido) {
-//        this.mapaPedido = mapaPedido;
-//    }
-//
-//    public double getPrecio() {
-//        return precio;
-//    }
-//
-//    public void setPrecio(double precio) {
-//        this.precio = precio;
-//    }
-//}
+package org.example.PracticaMercadaw;
+
+import java.util.HashMap;
+
+public class Pedido {
+    HashMap<Producto,Integer>mapaPedido = new HashMap<>();
+    private double precio;
+
+    public Pedido() {
+        this.mapaPedido = new HashMap<>();
+        this.precio = 0;
+    }
+
+    public void actualizarImporteTotal(double importe){
+
+    }
+    public void aplicarPromo3x2(){
+
+    }
+    public void aplicarPromo10(){
+
+    }
+
+    public HashMap<Producto, Integer> getMapaPedido() {
+        return mapaPedido;
+    }
+
+    public void setMapaPedido(HashMap<Producto, Integer> mapaPedido) {
+        this.mapaPedido = mapaPedido;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
