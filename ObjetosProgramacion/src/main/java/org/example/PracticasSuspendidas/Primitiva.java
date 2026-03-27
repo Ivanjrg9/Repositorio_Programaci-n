56d303adb8fddaefb8536dce1809452f9f2ea882
F:\Ejercicios programacion <ramosgamarraivanjose@yahoo.es>
2026-03-27 11:28:37 +0100

    Practica primitiva

--- ObjetosProgramacion/src/main/java/org/example/PracticasSuspendidas/Primitiva.java
+++ ObjetosProgramacion/src/main/java/org/example/PracticasSuspendidas/Primitiva.java
@@ -1,6 +1,5 @@
 package org.example.PracticasSuspendidas;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,31 +13,37 @@ public class Primitiva {
         int reintegro;
         System.out.println("*** Sorteo de la primitiva ***");
 
-
         do {
             System.out.println("Selecciona seis numeros del 1 al 49 y un numero reintegro del 0 al 9:");
             System.out.println("Formato:  N-N-N-N-N-N/R");
-            String eleccionSorteoUsuario = teclado.nextLine();
-            String sorteoUsuarioVector[] = eleccionSorteoUsuario.split("[-/]");
+            String bolasSorteoUsuario = teclado.nextLine();
+            String bolasUsuarioVector[] = bolasSorteoUsuario.split("[-/]");
 
-            if (eleccionSorteoUsuario.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d")){
-                if (Integer.parseInt(Arrays.toString(sorteoUsuarioVector)) > 49 || Integer.parseInt(Arrays.toString(sorteoUsuarioVector) < 1)){
+            if (bolasSorteoUsuario.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}-\\d{1,2}/\\d")){
+                for (int i = 0; i < bolasUsuarioVector.length; i++) {
+                    int numeros = Integer.parseInt(bolasUsuarioVector[i]);
+                    if (numeros < 1 || numeros > 49){
+                        System.out.println("------------------------------");
+                        System.out.println("Error, debes introducir numeros entre 1 y 49...");
+                        System.out.println("------------------------------");
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < 6; i++) {
+                    bolasSorteo = aleatorio.nextInt(48)+1;
+                    System.out.println(bolasSorteo);
 
                 }
 
-
                 formatoCorrecto = true;
-
             }else {
                 System.out.println("------------------------------");
                 System.out.println("Error, formato incorrecto...");
                 System.out.println("------------------------------");
-
             }
 
-
         }while (!formatoCorrecto);
-
 
     }
 }
